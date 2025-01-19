d350d505ff8430963a1162a7a8bad2584b1e0fc1
Devansh Bajpai <devanshb.code@gmail.com>
2025-01-20 02:09:02 +0530

    completed basic backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
@@ -10,7 +11,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.render("index");
+    fs.readdir("./files_save", (err, files) => {
+
+        res.render("index", {files: files});
+    })
+})
+
+app.post('/create', (req, res) => {
+
+    fs.writeFile(`./files_save/${req.body.title.split(' ').join('') + '.txt'}`, req.body.content, (err) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/');
+        }
+        else {
+            res.redirect('/');
+        }
+    })
+
+    
 })
 
 app.listen(3000, () => {

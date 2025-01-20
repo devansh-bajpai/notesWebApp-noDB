c3c68f1a06b85f00a1812af4a3102d4b61cb6b46
Devansh Bajpai <devanshb.code@gmail.com>
2025-01-20 14:51:27 +0530

    completed majority functionings

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
+
     fs.readdir("./files_save", (err, files) => {
 
         res.render("index", {files: files});
@@ -30,6 +31,39 @@ app.post('/create', (req, res) => {
     })
 
     
+})
+
+app.get('/get/:filename', (req, res) => {
+    fs.readFile(`./files_save/${req.params.filename}`, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("show", {data: data, filename: req.params.filename});
+        }
+    });
+})
+
+
+app.post('/edit/:filename', (req, res) => {
+    if (req.body.renameFile != ''){
+        fs.rename(`./files_save/${req.params.filename}`, `./files_save/${req.body.renameFile}`, (err) => {
+            if(err) {
+                console.log(err);
+            }
+            console.log('Rename Complete!');
+        })
+    }
+    fs.writeFile(`./files_save/${req.params.filename}`, req.body.alterContent, (err) => {
+        if(err) {
+            console.log(err);
+            res.redirect('/');
+        }
+        else {
+            res.redirect('/');
+        }
+    })
+
 })
 
 app.listen(3000, () => {
